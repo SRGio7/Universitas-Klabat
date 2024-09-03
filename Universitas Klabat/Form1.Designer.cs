3a6c9107e8241782b5ef088b0bbed2556a1d44ec
Sergio Regar <117889913+SRGio7@users.noreply.github.com>
2024-09-04 00:46:50 +0800

    Add project files.

---
+++ Universitas Klabat/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Universitas_Klabat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            label3 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            radioButton1 = new RadioButton();
+            label6 = new Label();
+            radioButton2 = new RadioButton();
+            numericUpDown2 = new NumericUpDown();
+            label7 = new Label();
+            label8 = new Label();
+            numericUpDown3 = new NumericUpDown();
+            button2 = new Button();
+            button3 = new Button();
+            comboBox2 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(91, -125);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(369, 423);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(115, 185);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(115, 237);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(115, 162);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Nama";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(115, 215);
+            label2.Name = "label2";
+            label2.Size = new Size(37, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Usia";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(228, 535);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 39);
+            button1.TabIndex = 5;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(281, 287);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(154, 27);
+            dateTimePicker1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(279, 267);
+            label3.Name = "label3";
+            label3.Size = new Size(97, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Tanggal Lahir";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(281, 238);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(154, 27);
+            numericUpDown1.TabIndex = 10;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(281, 215);
+            label5.Name = "label5";
+            label5.Size = new Size(70, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Semester";
+            label5.Click += label5_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Informatika", "Sistem Informasi", "Teknologi Informasi" });
+            comboBox1.Location = new Point(115, 287);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(125, 28);
+            comboBox1.TabIndex = 12;
+            comboBox1.Text = "Jurusan";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(281, 162);
+            label4.Name = "label4";
+            label4.Size = new Size(33, 20);
+            label4.TabIndex = 14;
+            label4.Text = "NM";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(281, 185);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(154, 27);
+            textBox3.TabIndex = 13;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(115, 389);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(70, 24);
+            radioButton1.TabIndex = 15;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Kawin";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(115, 366);
+            label6.Name = "label6";
+            label6.Size = new Size(49, 20);
+            label6.TabIndex = 16;
+            label6.Text = "Status";
+            label6.Click += label6_Click;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(115, 419);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(74, 24);
+            radioButton2.TabIndex = 17;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Lajang";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(281, 342);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(150, 27);
+            numericUpDown2.TabIndex = 18;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(280, 321);
+            label7.Name = "label7";
+            label7.Size = new Size(118, 20);
+            label7.TabIndex = 19;
+            label7.Text = "Jumlah Keluarga";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(279, 377);
+            label8.Name = "label8";
+            label8.Size = new Size(92, 20);
+            label8.TabIndex = 21;
+            label8.Text = "Jumlah Anak";
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(280, 398);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(150, 27);
+            numericUpDown3.TabIndex = 20;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 632);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 22;
+            button2.Text = "Contact Us";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(123, 632);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 23;
+            button3.Text = "Feedback";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Informatika", "Sistem Informasi", "Teknologi Informasi" });
+            comboBox2.Location = new Point(115, 332);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(125, 28);
+            comboBox2.TabIndex = 24;
+            comboBox2.Text = "Gender";
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(551, 673);
+            Controls.Add(comboBox2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label8);
+            Controls.Add(numericUpDown3);
+            Controls.Add(label7);
+            Controls.Add(numericUpDown2);
+            Controls.Add(radioButton2);
+            Controls.Add(label6);
+            Controls.Add(radioButton1);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(comboBox1);
+            Controls.Add(label5);
+            Controls.Add(numericUpDown1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+        private NumericUpDown numericUpDown1;
+        private Label label5;
+        private ComboBox comboBox1;
+        private Label label4;
+        private TextBox textBox3;
+        private RadioButton radioButton1;
+        private Label label6;
+        private RadioButton radioButton2;
+        private NumericUpDown numericUpDown2;
+        private Label label7;
+        private Label label8;
+        private NumericUpDown numericUpDown3;
+        private Button button2;
+        private Button button3;
+        private ComboBox comboBox2;
+    }
+}
---
+++ Universitas Klabat/Form1.cs
@@ -0,0 +1,55 @@
+namespace Universitas_Klabat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
